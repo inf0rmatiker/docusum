1c4c6d115775a8d8c6e9684d9ab4daaa2fd21283
Caleb <ccarlson355@gmail.com>
2019-10-30 22:02:33 -0600

    Fix run script

--- run.sh
+++ run.sh
@@ -4,16 +4,17 @@
 hadoop="${HADOOP_HOME}/bin/hadoop"
 hdfs="${HADOOP_HOME}/bin/hdfs"
 
-mvn clean && mvn package
-if [[ "$1" == "demo" ]]; then
-  echo "Running demo..."
-  #${hdfs} dfs -rm -r -f /cs435/PA2demo/tmp /cs435/PA2demo/tmp2 /cs435/PA2demo/output
-  #${hadoop} jar target/docusum-1.0-SNAPSHOT.jar driver.ProfileA /cs435/PA2demo/inputfiles/
-  ${hdfs} dfs -rm -r -f /cs435/PA2demo/output
-  ${hadoop} jar target/docusum-1.0-SNAPSHOT.jar driver.ProfileB /cs435/PA2demo/inputfiles/ /cs435/PA2demo/output
+
+if [[ $# -ge 2 ]]; then
+  echo -e "Cleaning and building the runnable jar...\n"
+  mvn clean && mvn package 
+
+  echo -e "\nRemoving any intermediate output directories...\n"
+  ${hdfs} dfs -rm -r -f /tempOut1 /tempOut2 $2  
+
+  echo -e "\nRunning MapReduce jobs...\n"
+  ${hadoop} jar target/docusum-1.0-SNAPSHOT.jar driver.ProfileA $1
+  ${hadoop} jar target/docusum-1.0-SNAPSHOT.jar driver.ProfileB $1 $2
 else
-  echo "Running on 1 GB dataset"
-  ${hdfs} dfs -rm -r -f /cs435/pa2-profileB-out /cs435/tmp /cs435/tmp2 /cs435/pa2-profileA-out /cs435/pa2-temp-out
-  ${hadoop} jar target/docusum-1.0-SNAPSHOT.jar driver.ProfileA /cs435/PA2Dataset/
-  ${hadoop} jar target/docusum-1.0-SNAPSHOT.jar driver.ProfileB /cs435/PA2Dataset/ /cs435/pa2-profileB-out
+  echo "Usage: ./run.sh <input_path> <output_path>"
 fi
